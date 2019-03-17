e484982edee07c2157ca698653d38baf5f8d3d72
Drayoni <oludare.oni@konga.com>
2019-03-17 13:11:59 +0100

    code optimization

--- love.js
+++ love.js
@@ -7,6 +7,7 @@ const greatPeople = [
     knownFor: 'I took a pill in Ibiza, In the Arms of a stranger',
     quote: `"My smiles don\'t result from good things, they result in good things."`,
     profession: 'Singer',
+    source: 'img/posner.png',
   },
 
   {
@@ -15,6 +16,7 @@ const greatPeople = [
     knownFor: 'Theory of Relativity',
     quote: `"Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid."`,
     profession: 'Physicist',
+    source: 'img/albert.png',
   },
 
   {
@@ -23,22 +25,10 @@ const greatPeople = [
     knownFor: 'I.T.T. (International Thief Thief), Gentle man',
     quote: `"The music of Africa is big sound: it’s the sound of a community."`,
     profession: 'Singer and Activist',
+    source: 'img/fela.png',
   },
 ]
-
-const mike = greatPeople[0];
-const albert = greatPeople[1];
-const fela = greatPeople[2];
-
-textAnim = () => {
-  const sk1 = document.querySelector('.sk1');
-  const sk2 = document.querySelector('.sk2');
-
-  sk1.style.cssText = `animation: textAnim1 500ms;`
-  setTimeout(()=> {sk1.style.cssText = `animation: none;`}, 600);
-  sk2.style.cssText = `animation: textAnim1 500ms;`
-  setTimeout(()=> {sk2.style.cssText = `animation: none;`}, 600);
-}
+const loveImg = document.querySelector('.loveImg');
 
 user = (personObject) => {
   const skill = document.querySelector('#skill');
@@ -49,52 +39,46 @@ user = (personObject) => {
   skill.innerText = personObject.skill;
   knownFor.innerText = personObject.knownFor;
   quote.innerText =personObject.quote;
+  loveImg.children[0].src = personObject.source;
+  profession.innerText = personObject.profession;
+
   for (names of name ) { 
     names.innerText = personObject.name;
     names.style.cssText = `animation: textAnim 200ms;`
     setTimeout(()=> {names.style.cssText = `animation: none;`}, 200);
   }
 
+  textAnim = () => {
+    const sk1 = document.querySelector('.sk1');
+    const sk2 = document.querySelector('.sk2');
+  
+    sk1.style.cssText = `animation: textAnim1 500ms;`
+    setTimeout(()=> {sk1.style.cssText = `animation: none;`}, 600);
+    sk2.style.cssText = `animation: textAnim1 500ms;`
+    setTimeout(()=> {sk2.style.cssText = `animation: none;`}, 600);
+  }
+
   textAnim();
 
-  profession.innerText = personObject.profession;
 }
 
 presentation = () => {
-  const loveImg = document.querySelectorAll('.loveImg');
   const next = document.querySelectorAll('.next');
   const previous = document.querySelectorAll('.previous');
 
   let currentStep = 0;
-  loveImg[currentStep].classList.add("active");
 
   nextAction = () => {
     active('next');
-    for(img of loveImg) {
-      if(img.classList.contains('active')) {
-        if(img.children[0].alt === 'posner') user(mike);
-        else if(img.children[0].alt === 'albert') user(albert);
-        else if(img.children[0].alt === 'fela') user(fela);
-      }  
-    }
   }
 
   previousAction = () => {
     active('previous');
-    for(img of loveImg) {
-      if(img.classList.contains('active')) {
-        if(img.children[0].alt === 'posner') user(mike);
-        else if(img.children[0].alt === 'albert') user(albert);
-        else if(img.children[0].alt === 'fela') user(fela);
-      }
     }
-  }
 
   active = (direction) => {
-    loveImg[currentStep].classList.remove('active');
-  
     if(direction == "next") {
-      if(currentStep < loveImg.length - 1) currentStep++;
+      if(currentStep < greatPeople.length - 1) currentStep++;
       else{
         currentStep = 0;
       }
@@ -104,22 +88,25 @@ presentation = () => {
       if(currentStep > 0) currentStep--;
       
       else{
-        currentStep = loveImg.length - 1;
+        currentStep = greatPeople.length - 1;
       }
     }
   
     showCurrent(currentStep);
-    // setTimeout(()=>{run()}, 500);
   }
   
   const showCurrent = (position) => {
-    loveImg[position].classList.add("active");
+    loveImg.classList.add('active');
+    user(greatPeople[position]);
+    setTimeout(()=>{loveImg.classList.remove('active')}, 300);
   };
+
+  // next.addEventListener('click', nextAction);
+  // previous.addEventListener('click', previousAction);
 
   for (nexts of next ) { nexts.addEventListener('click', nextAction); };
   for (previouss of previous ) { previouss.addEventListener('click', previousAction); };
-
-};
+}
 
 presentation();
 
@@ -166,5 +153,3 @@ loveInfo = (love, loveSprayer) => {
 loveSprayer.addEventListener('click', run);
 
 run();
-user(mike);
-    
